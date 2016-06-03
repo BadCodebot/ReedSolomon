828a6a466c46bb082d959a555e7c794fcba3ac6c
Michael Battle <mdb3000@gmail.com>
2016-06-03 16:37:14 -0700

    Added Polynomial class.

---
+++ Polynomial.cpp
@@ -0,0 +1,91 @@
+#include "Polynomial.h"
+#include <iostream>
+#include "GaloisField16.h"
+#include <algorithm>
+
+namespace SRFS {
+
+	Polynomial::Polynomial(int order) {
+		_order = order;
+		_coefficients = new uint16_t[order + 1];
+		memset(_coefficients, 0, (order + 1) * sizeof(uint16_t));
+	}
+
+	Polynomial::Polynomial(const Polynomial& x) {
+		_order = x._order;
+		_coefficients = new uint16_t[_order + 1];
+		memcpy(_coefficients, x._coefficients, (_order + 1)*sizeof(uint16_t));
+	}
+
+	Polynomial::~Polynomial() {
+		delete[] _coefficients;
+	}
+
+	Polynomial::Polynomial(uint16_t* coefficients, int order) {
+		_order = order;
+		_coefficients = new uint16_t[order + 1];
+		memcpy(_coefficients, coefficients, (order + 1)*sizeof(uint16_t));
+	}
+
+	void Polynomial::operator=(const Polynomial& x) {
+		if (_order != x._order) {
+			delete[] _coefficients;
+			_order = x._order;
+			_coefficients = new uint16_t[_order + 1];
+		}
+		memcpy(_coefficients, x._coefficients, (_order + 1)*sizeof(uint16_t));
+	}
+
+	Polynomial Polynomial::Create(uint16_t a0) {
+		Polynomial p(0);
+		p[0] = a0;
+		return p;
+	}
+
+	Polynomial Polynomial::Create(uint16_t a0, uint16_t a1) {
+		Polynomial p(1);
+		p[0] = a0;
+		p[1] = a1;
+		return p;
+	}
+
+	Polynomial& Polynomial::operator *= (const Polynomial& x) {
+
+		if (x.GetOrder() == 0) {
+			for (int i = 0; i < _order; ++i) _coefficients[i] = GaloisField16::Multiply(_coefficients[i], x[0]);
+		}
+		else {
+			int newOrder = _order + x.GetOrder();
+			uint16_t* resultCoefficients = new uint16_t[newOrder + 1];
+			for (int i = newOrder; i >= 0; --i) {
+				resultCoefficients[i] = 0;
+
+				int low = std::max<int>(0, i - x.GetOrder());
+				int high = std::min<int>(i, _order);
+
+				for (int ai = low; ai <= high; ai++) {
+					resultCoefficients[i] = GaloisField16::Add(resultCoefficients[i], GaloisField16::Multiply(_coefficients[ai], x[i - ai]));
+				}
+			}
+			delete[] _coefficients;
+			_coefficients = resultCoefficients;
+			_order = newOrder;
+		}
+
+		return *this;
+	}
+
+	static Polynomial operator*(const Polynomial& a, const Polynomial& b) {
+
+		Polynomial result(a.GetOrder() + b.GetOrder());
+		for (int i = result.GetOrder(); i >= 0; --i) {
+
+			int low = std::max<int>(0, i - b.GetOrder());
+			int high = std::min<int>(i, a.GetOrder());
+
+			for (int ai = low; ai <= high; ai++) result[i] = GaloisField16::Add(result[i], GaloisField16::Multiply(a[ai], b[i - ai]));
+		}
+
+		return result;
+	}
+}
---
+++ Polynomial.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <cstdint>
+
+namespace SRFS {
+
+	class Polynomial {
+
+	public:
+
+		static Polynomial Create(uint16_t a0);
+		static Polynomial Create(uint16_t a0, uint16_t a1);
+
+		Polynomial(int order);
+
+		Polynomial(const Polynomial& x);
+
+		~Polynomial();
+
+		Polynomial(uint16_t* coefficients, int order);
+
+		void operator=(const Polynomial& x);
+
+		inline uint16_t operator[](int i) const { return _coefficients[i]; }
+
+		inline uint16_t& operator[](int i) { return _coefficients[i]; }
+
+		inline int GetOrder() const { return _order; }
+
+		Polynomial& operator *= (const Polynomial& x);
+
+	private:
+
+		// The index corresponds to the power of 'x'.  So, the polynomial '2x^2 + 3x + 4' would be stored as {4,3,2}
+		uint16_t* _coefficients;
+		int _order;
+	};
+
+	Polynomial operator*(const Polynomial& a, const Polynomial& b);
+}
